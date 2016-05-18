4eb77ddd3e32843e56c8ec31e5229936b9cb5345
IronHeart7334 <greengrappler12@gmail.com>
2016-05-18 13:38:57 -0700

    Added file

---
+++ sprite_function copy.js
@@ -0,0 +1,23 @@
+function sprite(map, colors) {
+	this.map = map;
+	this.colors = ["rgb(0, 0, 0)"];
+	this.colors = this.colors.concat(colors);
+}
+
+sprite.prototype = {
+
+	draw:function(x, y, size){
+		var canvas = document.getElementById("canvas");
+		var canvas = canvas.getContext("2d");
+        for (var row = 0; row < this.map.length; row++) {
+            for (var column = 0; column < this.map[row].length; column++){
+            	if (this.map[row][column] == 0){
+            		continue;
+            	}
+            		canvas.fillStyle = this.colors[this.map[column][row]];
+            		canvas.fillRect(x + row * size, y + column * size, size, size);
+            }	
+        }
+    }
+	
+}
