671288ff8deff0cbbbacc4ca20c11f3b3dfc5163
IronHeart7334 <greengrappler12@gmail.com>
2017-08-22 09:00:18 -0700

    Working on easy sprite creator

--- sprite_function.js
+++ sprite_function.js
@@ -4,9 +4,28 @@ function Sprite(map, colors) {
 	this.colors = ["rgb(0, 0, 0)"];
 	this.colors = this.colors.concat(colors);
 }
+Sprite.construct = function(){
+    var doc = document.body;
+    var numColumns = parseInt(prompt("How many columns do you want your sprite to have?"));
+    var numRows = parseInt(prompt("What about rows?"));
+    
+    var div;
+    
+    for (var column = 0; column < numColumns; column ++){
+        for (var row = 0; row < numRows; row ++){
+            div = document.createElement("div");
+            div.style.position = "absolute";
+            div.style.left = column * 50 + "px";
+            div.style.top = row * 50 + "px";
+            div.style.height = "50px";
+            div.style.width = "50px";
+            div.style.backgroundColor = "green";
+            doc.appendChild(div);
+        }
+    }
+}
 
 Sprite.prototype = {
-
 	draw:function(x, y, size){
 		var canvas = document.getElementById("canvas");
 		var canvas = canvas.getContext("2d");
