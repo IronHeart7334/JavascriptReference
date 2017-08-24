254966b7a259aba2baecddfe585e4b9195ea490a
IronHeart7334 <greengrappler12@gmail.com>
2017-08-24 09:14:50 -0700

    bugged: div does not change color

--- sprite_function.js
+++ sprite_function.js
@@ -1,26 +1,40 @@
+var doc;
+var p;
+
 // The Sprite Class
 function Sprite(map, colors) {
 	this.map = map;
 	this.colors = ["rgb(0, 0, 0)"];
 	this.colors = this.colors.concat(colors);
 }
+
+// clean up later
 Sprite.construct = function(){
-    var doc = document.body;
+    doc = document.body;
     var numColumns = parseInt(prompt("How many columns do you want your sprite to have?"));
     var numRows = parseInt(prompt("What about rows?"));
     
+    p = new ProtoSprite();
+    
+    var addColor = document.createElement("div");
+    addColor.style.position = "absolute";
+    addColor.style.left = "0px";
+    addColor.style.top = "0px";
+    addColor.style.height = "50px";
+    addColor.style.width = "100px";
+    addColor.style.backgroundColor = "yellow";
+    addColor.innerHTML = "Add a color";
+    addColor.onclick = p.addColor.bind(p);
+    doc.appendChild(addColor);
+    
+    // implement removeColor later
+    
     var div;
+    var pix;
     
     for (var column = 0; column < numColumns; column ++){
         for (var row = 0; row < numRows; row ++){
-            div = document.createElement("div");
-            div.style.position = "absolute";
-            div.style.left = column * 50 + "px";
-            div.style.top = row * 50 + "px";
-            div.style.height = "50px";
-            div.style.width = "50px";
-            div.style.backgroundColor = "green";
-            doc.appendChild(div);
+            pix = new Pixel(column * 50, row * 50 + 50);
         }
     }
 }
@@ -38,6 +52,49 @@ Sprite.prototype = {
             		canvas.fillRect(x + row * size, y + column * size, size, size);
             }	
         }
+    }	
+}
+
+function ProtoSprite(){
+    this.colors = ["rgba(255, 255, 255, 0.0)", "rgb(0, 0, 0)"];
+}
+ProtoSprite.prototype = {
+    addColor:function(){
+        var r = parseInt(prompt("How much red?"));
+        var g = parseInt(prompt("How much green?"));
+        var b = parseInt(prompt("How much blue?"));
+        this.colors.push("rgb(" + r + ", " + g + ", " + b + ")");
+        console.log(this.colors);
     }
-	
+}
+
+function Pixel(x, y){
+    this.x = x;
+    this.y = y;
+    this.colorNum = 1;
+    this.createDiv();
+}
+Pixel.prototype = {
+    createDiv:function(){
+        div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.left = this.x + "px";
+        div.style.top = this.y + "px";
+        div.style.height = "50px";
+        div.style.width = "50px";
+        div.style.backgroundColor = p.colors[this.colorNum];
+        div.id = this.x + " " + this.y;
+        div.onclick = this.changeColor.bind(this);
+        doc.appendChild(div);
+    },
+    changeColor:function(){
+        this.colorNum += 1;
+        console.log(this.colorNum);
+        if(this.colorNum == p.colors.length){
+            this.colorNum = 0;
+        }
+        var div = document.getElementById(this.x + " " + this.y);
+        div.backgroundColor = p.colors[this.colorNum];
+        console.log(div);
+    }
 }
