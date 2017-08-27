eb3ef163de521b4cd7ccc76d8891a9b5a86ab2f4
IronHeart7334 <greengrappler12@gmail.com>
2017-08-27 12:52:28 -0700

    That's all for now,
    Will add a few more features later, but is mostly complete.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A basic sprite creator<br/>
 Feel free to use it, just please give me credit :)<br/>
+TODO: removeColor(), how to video <br/>
 ~Matt Crow<br/>
--- sprite_function.js
+++ sprite_function.js
@@ -1,35 +1,42 @@
-// clean up this mess!
-// document better
-
-function construct(){
-    var w = parseInt(prompt("How many columns do you want your sprite to have?"));
-    var h = parseInt(prompt("What about rows?"));
-    
-    var p = new ProtoSprite(w, h);
-    document.getElementById("addColor").onclick = p.addColor.bind(p);
-    // implement removeColor later
-    document.getElementById("genSprite").onclick = p.generateCode.bind(p);
-    
-    var pix;
-    for (var column = 0; column < w; column ++){
-        for (var row = 0; row < h; row ++){
-            pix = new Pixel(column, row, p);
-        }
-    }
-}
-
 // The Sprite Class
 function Sprite(map, colors) {
+	/* map: a two-dimensional array of integers, 
+	ranging from 0 to the length of colors MINUS ONE.
+	
+	colors: an array of css colors surrounded by quote marks.
+	
+	EXAMPLE:
+	var s = new Sprite([
+	    [0, 1, 0],
+	    [1, 1, 1],
+	    [0, 1, 0]
+	],
+	    ["rgb(0, 255, 0)"]
+	]);
+	
+	s.draw(0, 0, 10);
+	
+	
+	will draw a green plus sign in the upper-left corner of
+	an element with the id 'canvas'. Each corner of the plus
+	will be completely transparent.
+	*/
 	this.map = map;
-	this.colors = ["rgb(0, 0, 0)"];
+	// default color is transparent
+	this.colors = ["rgba(255, 255, 255, 0)"];
 	this.colors = this.colors.concat(colors);
 }
 Sprite.prototype = {
+	/*
+	You need to have a canvas element with the id 'canvas' in 
+	order to draw Sprites on it.
+	*/
 	draw:function(x, y, size){
 		var canvas = document.getElementById("canvas");
 		canvas = canvas.getContext("2d");
         for (var row = 0; row < this.map.length; row++) {
             for (var column = 0; column < this.map[row].length; column++){
+            	// Only draw if not transparent
             	if (this.map[row][column] != 0){
             		canvas.fillStyle = this.colors[this.map[row][column]];
             		canvas.fillRect(x + column * size, y + row * size, size, size);
@@ -37,6 +44,20 @@ Sprite.prototype = {
             }	
         }
     },
+    
+    /*
+    Log data sends the code used to generate a Sprite to the
+    console as a string. To test it out, just copy what comes 
+    up in the console, and type in the console as shown
+    
+    var x = PASTE
+    
+    hit enter, then:
+    
+    x.draw(XCOORD, YCOORD, PIXELSIZE);
+    
+    And you should see your sprite on the screen.
+    */
     logData:function(){
     	var msg = "new Sprite([";
     	for(var line = 0; line < this.map.length; line++){
@@ -44,85 +65,23 @@ Sprite.prototype = {
     		msg += this.map[line];
     		msg += "], "
     	}
+    	// chop off the last space and comma
     	msg = msg.slice(0, -2);
     	msg += "], [";
-    	msg += this.colors;
+    	
+    	/* starts at 1 so as to not log transparent, 
+    	as it is automatically
+    	added during initialization.
+    	*/
+    	for(var colorNum = 1; colorNum < this.colors.length; colorNum++){
+    	    msg += '"';
+    	    msg += this.colors[colorNum];
+    	    msg += '", ';
+    	}
+    	msg = msg.slice(0, -2);
     	msg += "]);";
     	console.log(msg);
+    	
+    	alert("Your Sprite code is now ready! \n Copy it from the console.");
     }	
-}
-
-function ProtoSprite(width, height){
-    this.colors = ["rgba(255, 255, 255, 0.0)", "rgb(0, 0, 0)"];
-    this.w = width;
-    this.h = height;
-    this.pixels = [];
-}
-ProtoSprite.prototype = {
-    addColor:function(){
-        var r = parseInt(prompt("How much red?"));
-        var g = parseInt(prompt("How much green?"));
-        var b = parseInt(prompt("How much blue?"));
-        this.colors.push("rgb(" + r + ", " + g + ", " + b + ")");
-    },
-    generateCode:function(){
-        var map = new Array(this.h);
-        
-        for(var i2 = 0; i2 < this.h; i2++){
-            var row = new Array(this.w);
-            map[i2] = row;
-        }
-        
-        var curPix;
-        for(var i = 0; i < this.pixels.length; i++){
-            curPix = this.pixels[i];
-            map[curPix.y][curPix.x] = curPix.colorNum;
-        }
-        this.colors.shift();
-        var n = new Sprite(map, this.colors);
-        n.logData();
-        for(var pixel = 0; pixel < this.pixels.length; pixel++){
-        	this.pixels[pixel].deleteDiv();
-        }
-    }
-}
-
-function Pixel(baseX, baseY, assignTo){
-    this.x = baseX;
-    this.y = baseY;
-    this.colorNum = 1;
-    this.assignedTo = assignTo;
-    assignTo.pixels.push(this);
-    this.createDiv();
-}
-Pixel.prototype = {
-    createDiv:function(){
-    	var c = document.getElementById("canvas");
-    	var cw = parseInt(c.style.width.slice(0, -1)) / 100 * window.innerWidth;
-    	var ch = parseInt(c.style.height.slice(0, -1)) / 100 * window.innerHeight
-    	var w = cw / this.assignedTo.w;
-    	var h = ch / this.assignedTo.h;
-        div = document.createElement("div");
-        div.style.position = "absolute";
-        div.style.left = this.x * w + "px";
-        div.style.top = this.y * h + ch / 10 + "px";
-        div.style.width = w + "px";
-        div.style.height = h + "px";
-        div.style.backgroundColor = this.assignedTo.colors[this.colorNum];
-        div.id = this.x + " " + this.y;
-        div.onclick = this.changeColor.bind(this);
-        document.body.appendChild(div);
-    },
-    changeColor:function(){
-        this.colorNum += 1;
-        if(this.colorNum == this.assignedTo.colors.length){
-            this.colorNum = 0;
-        }
-        var div = document.getElementById(this.x + " " + this.y);
-        div.style.backgroundColor = this.assignedTo.colors[this.colorNum];
-    },
-    deleteDiv:function(){
-    	var div = document.getElementById(this.x + " " + this.y);
-    	div.parentNode.removeChild(div);
-    }
 }
