ab4f2e4442befe6ee0e3304db2dd99137c226ed9
IronHeart7334 <greengrappler12@gmail.com>
2019-08-13 14:56:15 -0700

    added mode option

--- public_html/scripts/fetcher.js
+++ public_html/scripts/fetcher.js
@@ -1,5 +1,5 @@
-function tryFetch(url){
-    fetch(url)
+function tryFetch(url, opt={}){
+    fetch(url, opt)
         .then((response)=>response.text()) //since text() returns a Promise, not a string
         .then((text)=>{
             $("#result").text(text);
@@ -14,6 +14,12 @@ function tryFetch(url){
 $("#fetch")
     .click(
         ()=>{
-            tryFetch($("#url")[0].value);
+            let options = {};
+            let modeGroup = $('input[name=mode]:checked');
+            if(modeGroup.length === 1){
+                options.mode = modeGroup.val();
+            }
+            tryFetch($("#url")[0].value, options);
+            console.log(options);
         }
     );
